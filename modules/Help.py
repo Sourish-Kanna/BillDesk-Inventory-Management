24ec7beb63a653bf846e6cae71217267045a6e83
Sourish Kanna <sourishkanna001@gmail.com>
2024-06-10 15:47:51 +0530

    Help window

--- modules/Help.py
+++ modules/Help.py
@@ -18,47 +18,25 @@ def Help():
     root.title("Help")
     root.focus_force()
     root.config(bg=bgcol)
-    pat2='.png'
     root.attributes('-topmost', True)
+    img = []
 
-    img0  = ImageTk.PhotoImage(Image.open(f"{pat1}0{pat2}").resize((652,401),Image.Resampling.LANCZOS))
-    img1  = ImageTk.PhotoImage(Image.open(f"{pat1}1{pat2}").resize((652,401),Image.Resampling.LANCZOS))
-    img2  = ImageTk.PhotoImage(Image.open(f"{pat1}2{pat2}").resize((652,401),Image.Resampling.LANCZOS))
-    img3  = ImageTk.PhotoImage(Image.open(f"{pat1}3{pat2}").resize((652,401),Image.Resampling.LANCZOS))
-    img4  = ImageTk.PhotoImage(Image.open(f"{pat1}4{pat2}").resize((652,401),Image.Resampling.LANCZOS))
-    img5  = ImageTk.PhotoImage(Image.open(f"{pat1}5{pat2}").resize((652,401),Image.Resampling.LANCZOS))
-    img6  = ImageTk.PhotoImage(Image.open(f"{pat1}6{pat2}").resize((652,401),Image.Resampling.LANCZOS))
-    img7  = ImageTk.PhotoImage(Image.open(f"{pat1}7{pat2}").resize((652,401),Image.Resampling.LANCZOS))
-    img8  = ImageTk.PhotoImage(Image.open(f"{pat1}8{pat2}").resize((652,401),Image.Resampling.LANCZOS))
-    img9  = ImageTk.PhotoImage(Image.open(f"{pat1}9{pat2}").resize((652,401),Image.Resampling.LANCZOS))
-    img10 = ImageTk.PhotoImage(Image.open(f"{pat1}10{pat2}").resize((652,401),Image.Resampling.LANCZOS))
-    img11 = ImageTk.PhotoImage(Image.open(f"{pat1}11{pat2}").resize((652,401),Image.Resampling.LANCZOS))
-    img12 = ImageTk.PhotoImage(Image.open(f"{pat1}12{pat2}").resize((652,401),Image.Resampling.LANCZOS))
-    img13 = ImageTk.PhotoImage(Image.open(f"{pat1}13{pat2}").resize((652,401),Image.Resampling.LANCZOS))
-    img14 = ImageTk.PhotoImage(Image.open(f"{pat1}14{pat2}").resize((652,401),Image.Resampling.LANCZOS))
-    img15 = ImageTk.PhotoImage(Image.open(f"{pat1}15{pat2}").resize((652,401),Image.Resampling.LANCZOS))
-    img16 = ImageTk.PhotoImage(Image.open(f"{pat1}16{pat2}").resize((652,401),Image.Resampling.LANCZOS))
-    img17 = ImageTk.PhotoImage(Image.open(f"{pat1}17{pat2}").resize((652,401),Image.Resampling.LANCZOS))
-    img18 = ImageTk.PhotoImage(Image.open(f"{pat1}18{pat2}").resize((652,401),Image.Resampling.LANCZOS))
-    img19 = ImageTk.PhotoImage(Image.open(f"{pat1}19{pat2}").resize((652,401),Image.Resampling.LANCZOS))
-    img20 = ImageTk.PhotoImage(Image.open(f"{pat1}20{pat2}").resize((652,401),Image.Resampling.LANCZOS))
-    img21 = ImageTk.PhotoImage(Image.open(f"{pat1}21{pat2}").resize((652,401),Image.Resampling.LANCZOS))
-
-    img = [img0,img21,img1,img2,img3,img4,img5,img6,img7,img8,img9,img10,img11,img12,img13,
-           img14,img15,img16,img17,img18,img19,img20]
-    
+    for i in range(22):
+        image = ImageTk.PhotoImage(Image.open(f"{pat1}{i}.png").resize((652,401),Image.Resampling.LANCZOS))
+        img.append(image)
+        
 
     def Exit():
         root.destroy()
 
     def Back():
         global ImgC
-        ImgC-=1
-        if ImgC==0:
+        ImgC -= 1
+        if ImgC == 0:
             button3.config(state="disabled")
             l10.config(image=img[ImgC])
             return
-        elif ImgC==len(img)-2:
+        elif ImgC == len(img)-2:
             button2.config(state="active")
             l10.config(image=img[ImgC])
         else:
@@ -66,12 +44,12 @@ def Help():
 
     def Next():
         global ImgC
-        ImgC+=1
-        if ImgC==len(img)-1:
+        ImgC += 1
+        if ImgC == len(img)-1:
             button2.config(state="disabled")
             l10.config(image=img[ImgC])
             return
-        elif ImgC==1:
+        elif ImgC == 1:
             button3.config(state="active")
             l10.config(image=img[ImgC])
         else:
